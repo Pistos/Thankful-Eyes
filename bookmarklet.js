d4724e4a4bc7bdbe268c7aed94f0c4854dc6b1c2
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-01-18 14:11:21 -0500

    Moved some common code into theyes_average_words_per_line().

--- bookmarklet.js
+++ bookmarklet.js
@@ -18,25 +18,27 @@ function theyes_load_js( uri, id ) {
     document.getElementsByTagName('head')[0].appendChild( script );
 }
 
-function theyes_word_count( el ) {
-    return jQuery(el).text().split( /\b[\s,\.-:;]*/ ).length;
+function theyes_average_words_per_line( e ) {
+    var word_count = jQuery( e ).text().split( /\b[\s,\.-:;]*/ ).length;
+    var line_height = parseInt( $(e).css( 'line-height' ) );
+    var line_count = $(e).innerHeight() / line_height;
+    return word_count / line_count;
 }
 
 function theyes_adjust_fonts( root ) {
     var theyes_c = 0;
     jQuery( 'div, p' ).each( function() {
         $(this).css( 'line-height', '1.3' );
-        var word_count = theyes_word_count( this );
-        var line_height = parseInt( $(this).css( 'line-height' ) );
-        var line_count = $(this).innerHeight() / line_height;
-        var avg_words_per_line = word_count / line_count;
-
-        while( avg_words_per_line > 12 ) {
+        var avg_words_per_line = theyes_average_words_per_line( this );
+        var adjusted = false;
+        while( avg_words_per_line > 13 ) {
+            adjusted = true;
             var font_size = parseInt( $(this).css( 'font-size' ) ) + 1;
             $(this).css( 'font-size', font_size + 'px' );
-            line_height = parseInt( $(this).css( 'line-height' ) );
-            line_count = $(this).innerHeight() / line_height;
-            avg_words_per_line = word_count / line_count;
+            avg_words_per_line = theyes_average_words_per_line( this );
+        }
+        if( adjusted ) {
+            theyes_c++;
         }
     } );
 }
